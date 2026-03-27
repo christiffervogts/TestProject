6a129ae36ca269fd2e93dcef3c0715a757e07f17
christiffervogts <christiffer.vogts221@gmail.com>
2026-03-27 09:38:57 -0400

    Just getting started

---
+++ TestProject/src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//this is my test project. this is not serious use. 
+		
+		System.out.println("Just haveing fun.");
+	}
+
+}
