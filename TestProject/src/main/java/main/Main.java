1e05243a5e923a9d28d572cab1bfc08f055ac82c
christiffervogts <christiffer.vogts221@gmail.com>
2026-03-28 11:28:49 -0400

    made a window useing the LWJGL I think I've learned enough

---
+++ TestProject/src/main/java/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		//this is my test project. this is not serious use. just haveing fun 
+		
+		System.out.println("Start LWJGL");
+
+		Run run = new Run();
+		
+		
+		
+		
+	}
+
+}
---
+++ TestProject/src/main/java/main/Run.java
@@ -0,0 +1,102 @@
+package main;
+
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+import java.nio.*;
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+
+public class Run {
+
+	private long window;
+
+	
+	public Run(){
+		init();
+		loop();
+		
+		glfwFreeCallbacks(window);
+		glfwDestroyWindow(window);
+		
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+	
+	public void init() {
+		
+		GLFWErrorCallback.createPrint(System.err).set();
+		
+		if(!glfwInit()) {
+			throw new IllegalStateException("Can't initialize");
+		}
+		
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		
+		window = glfwCreateWindow(300, 300, "HEllO WORLD", NULL, NULL);
+		if(window == NULL) {
+			throw new RuntimeException("Faild to make window");
+		}
+		
+		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+			if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+				
+				glfwSetWindowShouldClose(window, true);
+				
+			}
+		});
+		
+		try(MemoryStack stack = stackPush()){
+			IntBuffer pWidth = stack.mallocInt(1);
+			IntBuffer pHeight = stack.mallocInt(1);
+
+			glfwGetWindowSize(window, pWidth, pHeight);
+			
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+			
+			glfwSetWindowPos(window, (vidmode.width() - pWidth.get(0))/2, (vidmode.height() - pHeight.get(0))/2);
+			
+		}
+			glfwMakeContextCurrent(window);
+			
+			glfwSwapInterval(1);
+			
+			glfwShowWindow(window);
+		
+	}
+	public void loop() {
+		GL.createCapabilities();
+
+		while (!glfwWindowShouldClose(window)) {
+			float rand1 = (float) Math.random();
+			float rand2 = (float) Math.random();
+			float rand3 = (float) Math.random();
+			float rand4 = (float) Math.random();
+			glClearColor(rand1, rand2, rand3, rand4);
+		    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		    glBegin(GL_TRIANGLES);
+		    glColor3f(1.0f, 0.0f, 0.0f);
+		    glVertex2f(-0.5f, -0.5f);
+
+		    glColor3f(0.0f, 1.0f, 0.0f);
+		    glVertex2f(0.5f, -0.5f);
+
+		    glColor3f(0.0f, 0.0f, 1.0f);
+		    glVertex2f(0.0f, 0.5f);
+		    glEnd();
+
+		    glfwSwapBuffers(window);
+		    glfwPollEvents();
+		}
+	}
+	
+	
+}
